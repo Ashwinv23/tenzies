6063a80d803056c380cae34ce6ef5550d9b0b8c1
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-02-16 20:34:36 +0530

    added dice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import Dice from "./Dice";
 import "./App.css";
 
 function App() {
   return (
     <main className="outer">
-      <div className="inner"></div>
+      <div className="inner">
+        <div className="dice-container">
+          <Dice value={1} />
+          <Dice value={4} />
+          <Dice value={6} />
+          <Dice value={2} />
+          <Dice value={5} />
+          <Dice value={3} />
+          <Dice value={6} />
+          <Dice value={1} />
+          <Dice value={4} />
+          <Dice value={5} />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/Dice.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dice({ value }) {
+  return (
+    <div className="dice">
+      <h3>{value}</h3>
+    </div>
+  );
+}
+
+export default Dice;
